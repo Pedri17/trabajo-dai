e9ba41c352c5bc7e35933e8becba06f6cfc70f1b
civeira <93115179+civeira@users.noreply.github.com>
2023-10-10 12:00:36 +0200

    Add files via upload

--- src/main/java/es/uvigo/esei/dai/hybridserver/http/HTTPRequest.java
+++ src/main/java/es/uvigo/esei/dai/hybridserver/http/HTTPRequest.java
@@ -20,149 +20,185 @@ package es.uvigo.esei.dai.hybridserver.http;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.HashMap;
+import java.net.URLDecoder;
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.TreeMap;
 
 public class HTTPRequest {
-	
-	HTTPRequestMethod method;
-	String resourceChain;
-	String [] resourcePath;
-	String resourceName;
-	String httpVersion;
-	Map<String,String> headerParameters;
-	Map<String, String> resourceParameters;
-	String content;
-	int contentLength = 0;
-	
-  public HTTPRequest(Reader reader) throws IOException, HTTPParseException {
-	  BufferedReader bReader = new BufferedReader(reader);
-	  String [] lineWords =  bReader.readLine().split(" ");
-	  this.headerParameters = new LinkedHashMap<String, String>();
-	  this.resourceParameters = new LinkedHashMap<String, String>();
-	  
-	  if(lineWords==null) throw new HTTPParseException();
-	  
-	//Get HTTPRequest Method
-	  switch(lineWords[0]) {
-	  	case "GET":
-	  		this.method = HTTPRequestMethod.GET;
-	  		break;
-	  	case "CONNECT":
-	  		this.method = HTTPRequestMethod.CONNECT;
-	  		break;
-	  	case "DELETE":
-	  		this.method = HTTPRequestMethod.DELETE;
-	  		break;
-	  	case "HEAD":
-	  		this.method = HTTPRequestMethod.HEAD;
-	  		break;
-	  	case "OPTIONS":
-	  		this.method = HTTPRequestMethod.OPTIONS;
-	  		break;
-	  	case "POST":
-	  		this.method = HTTPRequestMethod.POST;
-	  		break;
-	  	case "PUT":
-	  		this.method = HTTPRequestMethod.PUT;
-	  		break;
-	  	case "TRACE":
-	  		this.method = HTTPRequestMethod.TRACE;
-	  		break;
-	  	default: 
-	  		System.err.println("HTTP Request Method not expected.");
-	  		throw new HTTPParseException();
-	  }
-	  
-	  this.resourceChain = lineWords[1];
-	  if(lineWords[1].length()>1) {
-		  this.resourcePath = new String [2];
-		  this.resourcePath[0] = lineWords[1].substring(1).split("/")[0];
-		  this.resourcePath[1] = lineWords[1].substring(1).split("/")[1].split("\\?")[0];
-	  }else {
-		  this.resourcePath = new String[0];
-	  }
-	  this.resourceName = lineWords[1].substring(1).split("\\?")[0];
-	  this.httpVersion = lineWords[2].substring(0, lineWords[2].length());
-	  
-	  lineWords =  bReader.readLine().split(" ");
-	  while(lineWords[0].length() > 0) {
-		  String header = lineWords[0].substring(0,lineWords[0].length()-1);
-		  String content = lineWords[1];
-		  this.headerParameters.put(header, content);
-		  lineWords =  bReader.readLine().split(" ");
-	  }
-	  String line = bReader.readLine();
-	  String message = "";
-	  String [] messages;
-	  
-	  while(line != null && !line.isEmpty()) {
-		  message += line;
-		  messages = line.split("&");
-		  for(int i=0; i<messages.length; i++) {
-			  this.resourceParameters.put(messages[i].split("=")[0], messages[i].split("=")[1]);
-		  }
-		  line = bReader.readLine();
-	  }
-	  if(!message.isEmpty()) {
-		  this.content = message;
-		  this.contentLength = message.length();
-	  }
-	  
+    // Declare instance variables
+    private BufferedReader reader;
+    private HTTPRequestMethod method;
+    private String resourceChain;
+    private String[] resourcePath;
+    private Map<String, String> resourceParameters;
+    private Map<String, String> resourceHeaderParameters;
+    private String resourceName;
+    private String version;
+    private String content;
+    private int contentLength;
 
-  }
+    // Constructor to initialize an HTTPRequest object
+    public HTTPRequest(Reader reader) throws IOException, HTTPParseException {
+        // Initialize the BufferedReader for reading HTTP requests
+        this.reader = new BufferedReader(reader);
 
-  public HTTPRequestMethod getMethod() {
-    return this.method;
-  }
-
-  public String getResourceChain() {
-    return this.resourceChain;
-  }
-
-  public String[] getResourcePath() {
-    return this.resourcePath;
-  }
-
-  public String getResourceName() {
-    return this.resourceName;
-  }
-
-  public Map<String, String> getResourceParameters() {
-    return this.resourceParameters;
-  }
-
-  public String getHttpVersion() {
-    return this.httpVersion;
-  }
-
-  public Map<String, String> getHeaderParameters() {
-    return this.headerParameters;
-  }
-
-  public String getContent() {
-    return this.content;
-  }
-
-  public int getContentLength() {
-    return this.contentLength;
-  }
-
-  @Override
-  public String toString() {
-    final StringBuilder sb = new StringBuilder().append(this.getMethod().name()).append(' ')
-      .append(this.getResourceChain()).append(' ').append(this.getHttpVersion()).append("\r\n");
-
-    for (Map.Entry<String, String> param : this.getHeaderParameters().entrySet()) {
-      sb.append(param.getKey()).append(": ").append(param.getValue()).append("\r\n");
+        // Call the initValues method to populate the object's values
+        initValues();
     }
 
-    if (this.getContentLength() > 0) {
-      sb.append("\r\n").append(this.getContent());
+    // Getter for the HTTP request method
+    public HTTPRequestMethod getMethod() {
+        return this.method;
     }
 
-    return sb.toString();
-  }
+    // Getter for the resource chain (URI)
+    public String getResourceChain() {
+        return this.resourceChain;
+    }
+
+    // Getter for the resource path (parts of the URI)
+    public String[] getResourcePath() {
+        return this.resourcePath;
+    }
+
+    // Getter for the resource name (part of the URI)
+    public String getResourceName() {
+        return this.resourceName;
+    }
+    
+ // Getter for the parameters parsed from the HTTP Request
+    public Map<String, String> getResourceParameters() {
+
+		return this.resourceParameters;
+	}
+
+
+    // Getter for the HTTP version
+    public String getHttpVersion() {
+        return this.version;
+    }
+
+    // Getter for the HTTP request headers (parameters)
+    public Map<String, String> getHeaderParameters() {
+        return this.resourceHeaderParameters;
+    }
+
+    // Getter for the content of the HTTP request body
+    public String getContent() {
+        return this.content;
+    }
+
+    // Getter for the length of the content in the HTTP request body
+    public int getContentLength() {
+        return this.contentLength;
+    }
+
+    // Override the toString method to provide a string representation of the HTTP request
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder(this.getMethod().name()).append(' ')
+                .append(this.getResourceChain()).append(' ').append(this.getHttpVersion()).append("\r\n");
+
+        // Append the HTTP request headers to the string
+        for (Map.Entry<String, String> param : this.getHeaderParameters().entrySet()) {
+            sb.append(param.getKey()).append(": ").append(param.getValue()).append("\r\n");
+        }
+
+        // Append the content of the HTTP request body, if it exists
+        if (this.getContentLength() > 0) {
+            sb.append("\r\n").append(this.getContent());
+        }
+
+        return sb.toString();
+    }
+
+    /**
+     * Retrieves information from an HTTP request.
+     *
+     * @throws IOException
+     * @throws HTTPParseException if the HTTP request is incorrect.
+     */
+    private void initValues() throws IOException, HTTPParseException {
+        // Split the first line of the request to obtain the method, resource chain, and version
+        String[] firstLine = this.reader.readLine().split(" ");
+
+        if (firstLine.length != 3) {
+            throw new HTTPParseException("Invalid HTTPRequest first line.");
+        }
+
+        // Set the HTTP request method, resource chain (URI), and version
+        this.method = HTTPRequestMethod.valueOf(firstLine[0]);
+        this.resourceChain = firstLine[1];
+        this.version = firstLine[2];
+
+        // Initialize the HTTP request header parameters as a LinkedHashMap to preserve order
+        this.resourceHeaderParameters = new LinkedHashMap<String, String>();
+        String line;
+
+        // Read lines containing HTTP request header parameters until an empty line is encountered
+        while ((line = this.reader.readLine()) != null && !line.matches("")) {
+            // Check that header parameters are in the correct format (key: value)
+            if (line.matches(".*:.*")) {
+                String[] headerParameters = line.split(": ");
+                this.resourceHeaderParameters.put(headerParameters[0], headerParameters[1]);
+
+                // If "Content-Length" is found, set the content length
+                if (headerParameters[0].equals("Content-Length")) {
+                    this.contentLength = Integer.parseInt(headerParameters[1]);
+                }
+
+            } else {
+                throw new HTTPParseException("Invalid HTTPRequest.");
+            }
+        }
+
+        String[] parameters = null;
+
+        // Check if query parameters are part of the requested resource chain (URI)
+        if (this.resourceChain.matches(".+\\?.+")) {
+            // Split the resource chain into resource name and query parameters
+            String[] splitResource = this.resourceChain.split("\\?");
+            this.resourceName = splitResource[0].substring(1);
+            parameters = splitResource[1].split("\\&");
+
+        } else {
+            this.resourceName = this.resourceChain.substring(1);
+
+            // Check if there is content in the HTTP request body
+            if (this.contentLength > 0) {
+                char[] contentArray = new char[this.contentLength];
+                if (this.reader.read(contentArray) != contentArray.length) {
+                    throw new HTTPParseException("Invalid content length.");
+                }
+
+                this.content = new String(contentArray);
+
+                // Decode the content if it has a specific content type
+                String type = this.resourceHeaderParameters.get("Content-Type");
+                if (type != null && type.startsWith("application/x-www-form-urlencoded")) {
+                    this.content = URLDecoder.decode(this.content, "UTF-8");
+                }
+                parameters = this.content.split("\\&");
+            }
+        }
+
+        // Initialize the HTTP request query parameters as a LinkedHashMap
+        this.resourceParameters = new LinkedHashMap<String, String>();
+
+        // Populate the query parameters map
+        if (parameters != null) {
+            String[] splitParameters;
+            for (int i = 0; i < parameters.length; i++) {
+                splitParameters = parameters[i].split("=");
+                this.resourceParameters.put(splitParameters[0], splitParameters[1]);
+            }
+        }
+
+        // Initialize the resource path with a length of 0 in case there is no path
+        resourcePath = new String[0];
+        if (!resourceName.equals("")) {
+            // Split the resource name into parts for the resource path
+            resourcePath = this.resourceName.split("\\/");
+        }
+    }
 }
--- src/main/java/es/uvigo/esei/dai/hybridserver/http/HTTPResponse.java
+++ src/main/java/es/uvigo/esei/dai/hybridserver/http/HTTPResponse.java
@@ -21,108 +21,133 @@ import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 public class HTTPResponse {
+    
+	// Define instance variables
     private HTTPResponseStatus status;
     private String version;
     private String content;
     private Map<String, String> parameters;
 
+    // Constructor to initialize the object
     public HTTPResponse() {
-        this.parameters = new HashMap<>();
+        // Initialize parameters as a LinkedHashMap to preserve insertion order
+        this.parameters = new LinkedHashMap<>();
     }
 
+    // Getter for the HTTP response status
     public HTTPResponseStatus getStatus() {
         return status;
     }
 
+    // Setter for the HTTP response status
     public void setStatus(HTTPResponseStatus status) {
         this.status = status;
     }
 
+    // Getter for the HTTP version
     public String getVersion() {
         return version;
     }
 
+    // Setter for the HTTP version
     public void setVersion(String version) {
         this.version = version;
     }
 
+    // Getter for the content of the HTTP response
     public String getContent() {
         return content;
     }
 
+    // Setter for the content of the HTTP response
     public void setContent(String content) {
         this.content = content;
     }
 
+    // Getter for the parameters (headers) of the HTTP response
     public Map<String, String> getParameters() {
         return parameters;
     }
 
+    // Add a parameter (header) to the HTTP response
     public void putParameter(String name, String value) {
         parameters.put(name, value);
     }
 
+    // Check if a parameter (header) exists in the HTTP response
     public boolean containsParameter(String name) {
         return parameters.containsKey(name);
     }
 
+    // Remove a parameter (header) from the HTTP response
     public String removeParameter(String name) {
         return parameters.remove(name);
     }
 
+    // Clear all parameters (headers) from the HTTP response
     public void clearParameters() {
         parameters.clear();
     }
 
+    // Get a list of parameter (header) names in the HTTP response
     public List<String> listParameters() {
+        // Return a new ArrayList containing the keys (parameter names)
         return new ArrayList<>(parameters.keySet());
     }
 
+    // Generate and write the HTTP response to a Writer
     public void print(Writer writer) throws IOException {
-        String responseString = this.version + " " + this.status.getCode() + " " + this.status.getStatus() + "\r\n";
+        int contentLength = (content != null) ? content.length() : 0;
+        StringBuilder responseBuilder = new StringBuilder();
 
-        if (this.content != null) {
-            // calculate content length 
-            int contentLength = this.content.length();
-            
-            // Add the Content-Length header
-            this.parameters.put("Content-Length", Integer.toString(contentLength));
+        // Append the HTTP version, status code, and status message to the response
+        responseBuilder.append(version)
+                      .append(" ")
+                      .append(status.getCode())
+                      .append(" ")
+                      .append(status.getStatus())
+                      .append("\r\n");
 
-            // Write the headings
-            for (Map.Entry<String, String> entry : this.parameters.entrySet()) {
-                responseString += entry.getKey() + ": " + entry.getValue() + "\r\n";
-            }
-
-            // Add a blank line to separate headers from content
-            responseString += "\r\n";
-
-            // add content 
-            responseString += this.content;
-        } else {
-            // If there is no content, just write the headers
-            for (Map.Entry<String, String> entry : this.parameters.entrySet()) {
-                responseString += entry.getKey() + ": " + entry.getValue() + "\r\n";
-            }
-
-            // Add a blank line
-            responseString += "\r\n";
+        if (contentLength > 0) {
+            // Add the Content-Length header if content exists
+            parameters.put("Content-Length", Integer.toString(contentLength));
         }
 
-        writer.write(responseString);
+        for (Map.Entry<String, String> entry : parameters.entrySet()) {
+            // Append each parameter (header) to the response
+            responseBuilder.append(entry.getKey())
+                          .append(": ")
+                          .append(entry.getValue())
+                          .append("\r\n");
+        }
+
+        // Append an empty line to separate headers from content
+        responseBuilder.append("\r\n");
+
+        if (contentLength > 0) {
+            // Append the content if it exists
+            responseBuilder.append(content);
+        }
+
+        // Write the generated response to the provided Writer
+        writer.write(responseBuilder.toString());
     }
+
+    // Override the toString method to provide a string representation of the HTTP response
     @Override
     public String toString() {
         try (final StringWriter writer = new StringWriter()) {
+            // Call the print method to generate the response and convert it to a string
             this.print(writer);
             return writer.toString();
         } catch (IOException e) {
+            // Handle unexpected I/O exceptions
             throw new RuntimeException("Unexpected I/O exception", e);
         }
     }
 }
-
